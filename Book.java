8fb77e1c94fbbe54fb8c3a47968155de68644173
ulize67 <15212352607@163.com>
2026-09-24 09:35:04 +0800

    first commit

---
+++ Book.java
@@ -0,0 +1,14 @@
+Class Book{
+    private String id;
+    private String title;
+    public Book(String id,String title){
+        this.id = id;
+        this.title = title;
+        }
+        public String getId(){
+        return id;
+        }
+        public String getTitle(){
+        return title;
+        }
+}
